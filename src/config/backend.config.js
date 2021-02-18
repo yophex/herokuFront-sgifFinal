e76b6a90454bcd8ecc4f2c35f68bf5436ad3ea16
yophex <seba_yaupe@hotmail.com>
2021-02-18 13:10:00 -0300

    todo configurado

--- src/config/backend.config.js
+++ src/config/backend.config.js
@@ -1,4 +1,4 @@
-const ipBackend = "http://localhost:3000";
+const ipBackend = "https://back-sgif.herokuapp.com";
 const apiBiblia = "https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter";
 const apiClima = "https://weatherapi-com.p.rapidapi.com/forecast.json";
 
